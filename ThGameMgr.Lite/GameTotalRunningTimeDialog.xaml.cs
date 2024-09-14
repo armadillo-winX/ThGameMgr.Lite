09b3c9eeb4f07b9482c1f3921607b859e540b43b
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-09-15 00:14:54 +0900

    GameTotalRunningTimeDialog 実装

---
+++ ThGameMgr.Lite/GameTotalRunningTimeDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace ThGameMgr.Lite
+{
+    /// <summary>
+    /// GameTotalRunningTimeDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class GameTotalRunningTimeDialog : Window
+    {
+        private string? _gameTotalRunningTime;
+
+        public string? GameTotalRunningTime
+        {
+            get
+            {
+                return _gameTotalRunningTime;
+            }
+
+            set
+            {
+                _gameTotalRunningTime = value;
+                if (!string.IsNullOrEmpty(value))
+                {
+                    GameTotalRunningTimeBlock.Text = value;
+                }
+            }
+        }
+
+        public GameTotalRunningTimeDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
