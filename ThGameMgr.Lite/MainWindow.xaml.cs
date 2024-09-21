075658dcca537662054ca38b911491aafe4bbd36
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-09-21 21:48:27 +0900

    初回起動時の挙動を変更

--- ThGameMgr.Lite/MainWindow.xaml.cs
+++ ThGameMgr.Lite/MainWindow.xaml.cs
@@ -97,14 +97,24 @@ namespace ThGameMgr.Lite
             //メインウィンドウの設定構成処理はすべての設定が構成された後に行う
             if (Directory.Exists(PathInfo.SettingsDirectory))
             {
-                try
+                if (File.Exists(PathInfo.GamePathSettingsFile))
                 {
-                    SettingsConfigurator.ConfigureGamePathSettings();
+                    try
+                    {
+                        SettingsConfigurator.ConfigureGamePathSettings();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"ゲームパス設定の構成処理に失敗しました。\n{ex.Message}", "エラー",
+                            MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    MessageBox.Show($"ゲームパス設定の構成処理に失敗しました。\n{ex.Message}", "エラー",
-                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    MessageBox.Show("ゲームのパスを設定してください。", VersionInfo.AppName,
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    GamePathSettingsDialog gamePathSettingsDialog = new();
+                    gamePathSettingsDialog.ShowDialog();
                 }
 
                 try
@@ -125,6 +135,11 @@ namespace ThGameMgr.Lite
                 {
                     if (!string.IsNullOrEmpty(PathInfo.SettingsDirectory))
                         Directory.CreateDirectory(PathInfo.SettingsDirectory);
+
+                    MessageBox.Show("ゲームのパスを設定してください。", VersionInfo.AppName,
+                        MessageBoxButton.OK, MessageBoxImage.Information);
+                    GamePathSettingsDialog gamePathSettingsDialog = new();
+                    gamePathSettingsDialog.ShowDialog();
                 }
                 catch (Exception ex)
                 {
