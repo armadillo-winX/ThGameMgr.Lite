698c9f780a0d7a0095891da2473806ccfd274a62
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-10-14 20:30:43 +0900

    IsAdmin()実装

--- ThGameMgr.Lite/App.xaml.cs
+++ ThGameMgr.Lite/App.xaml.cs
@@ -9,6 +9,15 @@ namespace ThGameMgr.Lite
     /// </summary>
     public partial class App : Application
     {
+        public static bool IsAdmin()
+        {
+            //GetCurrentメソッドで現在のユーザーを取得
+            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            //System.Security.Principal.WindowsPrincipalを作成このオブジェクトは権限の確認ができる
+            System.Security.Principal.WindowsPrincipal principal = new(identity);
+
+            return principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+        }
     }
 
 }
